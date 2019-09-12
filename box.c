a0e061b603b30fd998bf4ebca3e96e78ae52e773
Samuel Christopher <thesamchris@gmail.com>
2019-09-12 16:32:54 +0800

    Completed and tested exercise two

--- box.c
+++ box.c
@@ -7,22 +7,85 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
-// Write your function prototypes below (and remove this comment!)
+int computeSurfaceArea();
+double computeDiagonal();
 
 int main(void) {
+	int length, width, height;
 
 	printf("Enter length: ");
+	scanf("%d", &length);
 
 	printf("Enter width : ");
+	scanf("%d", &width);
 
 	printf("Enter height: ");
+	scanf("%d", &height);
 
-	// fill in your code below (and remove this comment!)
+	printf("Surface area = %d\n", computeSurfaceArea(length, width, height));
+	printf("Diagonal = %.2f\n", computeDiagonal(length, width, height));
+
 
 	return 0;
 }
 
-// Add your functions below (and remove this comment!)
-// Every function should be preceded with a comment
-// describing what the function does.
+// computeSurfaceArea() takes in the length, witdth and height of a box and returns its surface area
+int computeSurfaceArea(int length, int width, int height) {
+	int surfaceArea = 2 * ((length * height) + (length * width) +(height * width));
+	return surfaceArea;
+}
+
+// computeDiagonal() takes in the length, width and height of a box and returns the diagonal between two vertices of the box that are furthest apart
+double computeDiagonal(int length, int width, int height) {
+	// first find the largest, second largest and smallest dimensions
+	int largest, second, smallest;
+	largest = 0;
+	second = 0;
+	smallest = 0;
+
+	if (length > largest) {
+		largest = length;
+		if (width > height) {
+			second = width;
+			smallest = height;
+		} else {
+			second = height;
+			smallest = width;
+		}
+	}
+	
+	if (width > largest) {
+		largest = width;
+		if (length > height) {
+			second = length;
+			smallest = height;
+		} else {
+			second = height;
+			smallest = length;
+		}
+	}
+
+	if (height > largest) {
+		largest = height;
+		if (length > width) {
+			second = length;
+			smallest = width;
+		} else {
+			second = width;
+			smallest = length;
+		}
+	}
+
+	// calculate diagonal by
+	// 1. calculate the base diagonal using largest and shortest
+	// 2. calculate the diagoanl between the vertices that are furthest apart using the base diagonal and second
+
+	double baseDiagonalSquared, diagonal;
+
+	baseDiagonalSquared = pow(largest, 2) + pow(smallest, 2);
+	diagonal = sqrt(baseDiagonalSquared + pow(second, 2));
+
+	return diagonal;
+}
