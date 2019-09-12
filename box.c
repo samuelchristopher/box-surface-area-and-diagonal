c0fb32037948df5a31b27b6f6f21f8acb48ac559
Samuel Paul Christopher <thesamchris@gmail.com>
2019-09-12 15:57:03 +0800

    initial commit

---
+++ box.c
@@ -0,0 +1,28 @@
+/**
+ * CS1010 AY2017/8 Semester 1 Lab1 Ex2
+ * box.c
+ * This program reads three positive integers representing the length, width and height of a box, and computes (1) its surface area, and (2) the length of the diagonal between two vertices of the box that are furthest apart.
+ * Samuel Paul Christopher
+ * C08
+ */
+
+#include <stdio.h>
+
+// Write your function prototypes below (and remove this comment!)
+
+int main(void) {
+
+	printf("Enter length: ");
+
+	printf("Enter width : ");
+
+	printf("Enter height: ");
+
+	// fill in your code below (and remove this comment!)
+
+	return 0;
+}
+
+// Add your functions below (and remove this comment!)
+// Every function should be preceded with a comment
+// describing what the function does.
